176d2720c0b980565bc709002761336ad13c5a1f
Alexei Pancratov <drivergear@gmail.com>
2022-06-04 13:01:57 -0400

    Added backend interacting with smart contract

---
+++ backend/server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const config = require("./config.json");
+const secret = require("./secret.json");
+const fs = require("fs");
+const Web3 = require("web3");
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/gamePurchases", async (req, res) => {
+  // Mint game copy NFT.
+  const web3 = new Web3();
+  web3.setProvider(new web3.providers.HttpProvider("http://localhost:8545"));
+
+  const abi = JSON.parse(fs.readFileSync("abi.json", "utf8"));
+  const gameCopyContract = new web3.eth.Contract(abi, config.contractAddress);
+
+  const tx = gameCopyContract.methods.buyGameCopy(
+    req.body.player,
+    "https://ipfs.io/ipfs/QmP535DzTS9ALkDfwA6rcASMmnoB1w3HysRbBktx6Y6osY"
+  );
+  const gas = await tx.estimateGas({ from: config.fromAddress });
+  const gasPrice = await web3.eth.getGasPrice();
+  const data = tx.encodeABI();
+
+  const signedTx = await web3.eth.accounts.signTransaction(
+    {
+      to: gameCopyContract.options.address,
+      data,
+      gas,
+      gasPrice,
+      data,
+    },
+    secret.privateKey
+  );
+
+  try {
+    const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+    console.log(`Transaction hash: ${receipt.transactionHash}`);
+    res.sendStatus(200);
+  } catch (e) {
+    console.error("An error has occurred.", e);
+    res.sendStatus(400);
+  }
+});
+
+app.listen(config.serverPort, () => {
+  console.log(`Example app listening at http://localhost:${config.serverPort}`);
+});
