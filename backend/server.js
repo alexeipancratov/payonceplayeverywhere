1f56be338a78af19e78c769637a845d04c1988d4
Alexei Pancratov <drivergear@gmail.com>
2022-06-04 18:58:05 -0400

    Implemented upload to IPFS

--- backend/server.js
+++ backend/server.js
@@ -5,49 +5,75 @@ const config = require("./config.json");
 const secret = require("./secret.json");
 const fs = require("fs");
 const Web3 = require("web3");
+const Ipfs = require("./ipfs");
 
 app.use(cors());
 app.use(express.json());
 
-app.post("/gamePurchases", async (req, res) => {
-  // Mint game copy NFT.
-  const web3 = new Web3();
-  web3.setProvider(new web3.providers.HttpProvider("http://localhost:8545"));
-
-  const abi = JSON.parse(fs.readFileSync("abi.json", "utf8"));
-  const gameCopyContract = new web3.eth.Contract(abi, config.contractAddress);
-
-  const tx = gameCopyContract.methods.buyGameCopy(
-    req.body.player,
-    "https://ipfs.io/ipfs/QmP535DzTS9ALkDfwA6rcASMmnoB1w3HysRbBktx6Y6osY"
-  );
-  const gas = await tx.estimateGas({ from: config.fromAddress });
-  const gasPrice = await web3.eth.getGasPrice();
-  const data = tx.encodeABI();
-
-  const signedTx = await web3.eth.accounts.signTransaction(
-    {
-      to: gameCopyContract.options.address,
-      data,
-      gas,
-      gasPrice,
-      data,
-    },
-    secret.privateKey
-  );
-
+async function uploadReceiptToIpfs(playerAddress, receipt) {
   try {
-    const receipt = await web3.eth.sendSignedTransaction(
-      signedTx.rawTransaction
+    const addResult = await Ipfs.ipfsClient.add(JSON.stringify(receipt, null, 2));
+    console.log("Uploaded receipt", addResult.cid);
+
+    return addResult.cid;
+  } catch (err) {
+    console.log("Failed to upload file to the IPFS", err);
+    throw err;
+  }
+}
+
+async function mintGameCopyNft(playerAddress, ipfsCid) {
+  try {
+    const web3 = new Web3();
+    web3.setProvider(new web3.providers.HttpProvider(config.blockchainAddress));
+
+    const abi = JSON.parse(fs.readFileSync("abi.json", "utf8"));
+    const gameCopyContract = new web3.eth.Contract(abi, config.contractAddress);
+
+    const tokenUri = `https://ipfs.io/ipfs/${ipfsCid}`;
+    const tx = gameCopyContract.methods.buyGameCopy(playerAddress, tokenUri);
+    const gas = await tx.estimateGas({ from: config.fromAddress });
+    const gasPrice = await web3.eth.getGasPrice();
+    const data = tx.encodeABI();
+
+    const signedTx = await web3.eth.accounts.signTransaction(
+      {
+        to: gameCopyContract.options.address,
+        data,
+        gas,
+        gasPrice,
+        data,
+      },
+      secret.privateKey
     );
+
+    const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
     console.log(`Transaction hash: ${receipt.transactionHash}`);
-    res.sendStatus(200);
-  } catch (e) {
-    console.error("An error has occurred.", e);
-    res.sendStatus(400);
+
+    return tokenUri;
+  } catch (err) {
+    console.error("Failed to mint game copy NFT.", err);
+    throw err;
+  }
+}
+
+app.post("/gamePurchases", async (req, res) => {
+  try {
+    const playerAddress = req.body.player;
+    const gameCopyReceipt = req.body.receipt;
+    const ipfsCid = await uploadReceiptToIpfs(playerAddress, gameCopyReceipt);
+    const tokenUri = await mintGameCopyNft(playerAddress, ipfsCid);
+
+    res.status(200);
+    res.send({
+      tokenUri,
+    });
+  } catch (err) {
+    res.status(400);
+    res.send(err.message);
   }
 });
 
 app.listen(config.serverPort, () => {
-  console.log(`Example app listening at http://localhost:${config.serverPort}`);
+  console.log(`App listening on port ${config.serverPort}`);
 });
